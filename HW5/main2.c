b240f3e9358c4ad49bd18d2ef24796e31e1ec30f
Alex Tsai <alex93.tsai@gmail.com>
2024-12-08 20:51:41 +0800

    Update main2.c

--- HW5/main2.c
+++ HW5/main2.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int main()
+{
+    double pi = 0.0;
+    int count = 1;
+
+    for (int i = 1; i < 300000; i += 2) { // 分母為奇數遞增
+        if (count % 2 == 0) {
+            pi -= 4.0 / i;  // 確保浮點數除法
+        } else {
+            pi += 4.0 / i;
+        }
+        count += 1;
+    }
+
+    printf("Pi approximation: %.5f\n", pi);
+    return 0;
+}
