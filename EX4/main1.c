7f873216add21fdd3cd2eadbb0d1bed26b777e18
Alex Tsai <alex93.tsai@gmail.com>
2024-12-12 15:30:16 +0800

    Update main1.c

--- EX4/main1.c
+++ EX4/main1.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main()
+{
+    int n = 7;
+    for (int i = 0; i < n + 1; i++){
+        for (int j = 0; j < n + 1; j++){
+            if (((n + 1) - i )<=  j){
+                printf("%d", i);
+                printf(" ");
+            }else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
